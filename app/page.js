aa79b415039ee7627f03396bea2d1e6ae0a2144f
Lupavinc <lupavinc@gmail.com>
2025-08-18 23:19:52 -0600

    Add files via upload

--- app/page.js
+++ app/page.js
@@ -54,15 +54,9 @@ export default function Home() {
         </h1>
 
           
-          //<div className="w-16 h-1 bg-yellow-400 my-4 mx-auto sm:mx-0"></div>
+          <div className="w-16 h-1 bg-yellow-400 my-4 mx-auto sm:mx-0"></div>
 
-          {/* Tagline paragraph removed */}
-
-          //<Button
-          //  text="Browse Listings"
-          //  onClick={handleButtonClick}
-          ///>
-        //</div>
+        </div>
       </section>
     </div>
   );
